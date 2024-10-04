2192bc53cec6ee7370a641c19fac3cbbe0203bc8
Samuel Adegboyega <--global>
2024-10-04 09:51:06 +0100

    Using Supervision Library for Anotation

--- Scripts/CAR DETECTION TESTING.py
+++ Scripts/CAR DETECTION TESTING.py
@@ -7,14 +7,21 @@ import matplotlib.pyplot as plt
 import time
 
 import ultralytics
-ultralytics.checks()
+#ultralytics.checks()
 from ultralytics import YOLO
 import time
 
+import supervision as sv
+
 WEIGTH_PATH=r'Weights\YoloV8CarDetection.pt'
 VIDEO_PATH=r'Videos\Cars Moving On Road Stock Footage - Free Download.mp4'
+
+box_annotator = sv.BoxAnnotator()
+label_annotator = sv.LabelAnnotator()
+
 model=YOLO(WEIGTH_PATH).to('cuda')
 cap = cv2.VideoCapture(VIDEO_PATH)
+
 
 text="{}"
 
@@ -29,19 +36,34 @@ while True:
    if not ret:
       print("Can't receive frame (stream end?) and end of frame reached. Exiting ...")
       break
+  
+   detections=model.predict(frame)[0]
+   #print(detections)
    
-   detections=model.predict(frame)
+   detections = sv.Detections.from_ultralytics(detections)
+  
+   labels = [
+    f"{class_name} {confidence:.2f}"
+    for class_name, confidence
+    in zip(detections['class_name'], detections.confidence)
+    ]
    
-   for detection in detections:
+   annotated_image = box_annotator.annotate(
+                                             scene=frame, detections=detections)
+   annotated_image = label_annotator.annotate(
+                                             scene=annotated_image, detections=detections, labels=labels)
+   
+   # for detection in detections:
       
-        for box in detection.boxes.data:
+   #      for box in detection.boxes.data:
             
-            (startX, startY, endX, endY,conf,cls) = box.cpu().numpy().astype(int).tolist()
+   #          (startX, startY, endX, endY,conf,cls) = box.cpu().numpy().astype(int).tolist()
             
-            cv2.rectangle(frame, (startX, startY), (endX, endY),(0, 128, 0), 1)
-            cv2.putText(frame, text.format(model.names[cls]), (startX, startY),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 0), 1)
-        
-   cv2.imshow('Video Stream',frame)
+   #          cv2.rectangle(frame, (startX, startY), (endX, endY),(0, 128, 0), 1)
+   #          cv2.putText(frame, text.format(model.names[cls]), (startX, startY),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 0), 1)
+   
+
+   cv2.imshow('Video Stream',annotated_image)
  
    
    if cv2.waitKey(1) == ord('q'):
